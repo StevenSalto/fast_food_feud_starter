71464cb419194ad202dd7846ed3908c3c9be20c3
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-06-14 23:52:33 -0400

    Finished all steps

--- src/components/NutritionalLabel/NutritionalLabel.jsx
+++ src/components/NutritionalLabel/NutritionalLabel.jsx
@@ -6,10 +6,17 @@ export function NutritionalLabel(props) {
   return (
     <div className="nutritional-label">
       <h3 className="title">Nutrition Facts</h3>
+      {console.log("nutritionalLabel props: ", props.item)}
+      {console.log("Nut facts import array: ", nutritionFacts)}
+      <h4 className="item-name">{props.item.item_name}</h4>
 
-      <h4 className="item-name">{console.log("in nutlabel: ",props.item)}</h4>
-
-      <ul className="fact-list">{/* WRITE CODE HERE */}</ul>
+      <ul className="fact-list">{
+        console.log("making facts.........")}{
+      nutritionFacts.map(
+        (elem) => ( 
+        <NutritionalLabelFact key={elem.id} label={elem.label} attribute={elem.attribute} item={props.item}/>
+        )
+      )}</ul>
     </div>
   )
 }
@@ -17,8 +24,9 @@ export function NutritionalLabel(props) {
 export function NutritionalLabelFact(props) {
   return (
     <li className="nutrition-fact">
-      <span className="fact-label">{/* WRITE CODE HERE */}</span>{" "}
-      <span className="fact-value">{/* WRITE CODE HERE */}</span>
+      {console.log("nutritionallabelfacts props: ", props)}
+      <span className="fact-label">{props.label}</span>{" "}
+      <span className="fact-value">{props.item[props.attribute]}</span>
     </li>
   )
 }
